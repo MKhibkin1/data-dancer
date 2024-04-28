fd9b20241e50195ee1a080fdf7a8b778d7abf4b9
MikhailKhibkin <mkhibkin1@gmail.com>
2024-04-28 13:31:43 -0400

    initial commit

---
+++ src/main/java/com/example/datadancer/DataDancerApplication.java
@@ -0,0 +1,13 @@
+package com.example.datadancer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataDancerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataDancerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/datadancer/DataDancerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datadancer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataDancerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
